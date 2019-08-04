e42386a7348bbb9942b3eb3473a19f069276dd34
TacomaJapanTown <52140649+TacomaJapanTown@users.noreply.github.com>
2019-08-04 16:48:26 -0700

    Add files via upload

--- dailypaths.js
+++ dailypaths.js
@@ -65,7 +65,7 @@ var MasayeJinguji = [
 ];
 
 //Making the map
-var mymap = L.map('mapid').setView([47.256105, -122.443722], 15);
+var mymap = L.map('mapid').setView([47.256105, -122.443722], 14);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',

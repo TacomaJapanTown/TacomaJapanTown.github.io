57a208d08ec97e74883f007a22e5f889ab578ec6
TacomaJapanTown <52140649+TacomaJapanTown@users.noreply.github.com>
2019-08-05 15:34:29 -0700

    Add files via upload

--- dailypaths.js
+++ dailypaths.js
@@ -78,46 +78,48 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 //The pause/play function
 function pauseButtonClick()
 {
-    var markerList = [marker2, marker3, marker4, marker5, marker6];
-    for (i in markerList)
-    {
-        if (markerList[i].isPaused())
-            markerList[i].start();
-        else
-            markerList[i].pause();
-    }
+  var markerList = [marker2, marker3, marker4, marker5, marker6];
+  for (i in markerList)
+  {
+    if (markerList[i].isPaused())
+    markerList[i].start();
+    else
+    markerList[i].pause();
+  }
 }
 
 //loading the geojson of homes along the paths
 $.getJSON("data/pathstopshomes.geojson",function(data){
   L.geoJson(data, {
     style: function (feature) {
-        return {color: feature.properties.color};
+      return {color: feature.properties.color};
     },
     onEachFeature: function (feature, layer) {
-        layer.bindPopup('<p>Name: '+feature.properties.Name+'</p><p>Address: '+feature.properties.Address+'</p><p>Notes: '+feature.properties.Notes+'</p><p>Source: '+feature.properties.Source+'</p>');
+      layer.bindPopup('<p>Name: '+feature.properties.Name+'</p><p>Address: '+feature.properties.Address+'</p><p>Notes: '+feature.properties.Notes+'</p><p>Source: '+feature.properties.Source+'</p>');
     }
-}).addTo(mymap);
+  }).addTo(mymap);
 
-//loading the geojson of schools along the paths
-$.getJSON("data/pathstopsschools.geojson",function(data){
-  L.geoJson(data, {
-    style: function (feature) {
+  //loading the geojson of schools along the paths
+  $.getJSON("data/pathstopsschools.geojson",function(data){
+    L.geoJson(data, {
+      style: function (feature) {
         return {color: feature.properties.color};
-    },
-    onEachFeature: function (feature, layer) {
+      },
+      onEachFeature: function (feature, layer) {
         layer.bindPopup('<p>Name: '+feature.properties.Name+'</p><p>Address: '+feature.properties.Address+'</p><p>Source: '+feature.properties.Source+'</p>');
-    }
-}).addTo(mymap);
+      }
+    }).addTo(mymap);
 
-//loading the geojson of the JLS
-$.getJSON("data/jls.geojson",function(data){
-  L.geoJson(data, {
-    style: function (feature) {
-        return {color: feature.properties.color};
-    },
-    onEachFeature: function (feature, layer) {
-        layer.bindPopup('<p>Name: '+feature.properties.Name+'</p><p>Address: '+feature.properties.Address+'</p><p>Source: '+feature.properties.Source+'</p><a href=https://www.loc.gov/item/wa0563/ target=_blank><img src=images/jls.jpg width=125em>');
-    }
-}).addTo(mymap);
-//I know it's not very technically efficient to load these seperate jsons but it was the quickest way for me
+    //loading the geojson of the JLS
+    $.getJSON("data/jls.geojson",function(data){
+      L.geoJson(data, {
+        style: function (feature) {
+          return {color: feature.properties.color};
+        },
+        onEachFeature: function (feature, layer) {
+          layer.bindPopup('<p>Name: '+feature.properties.Name+'</p><p>Address: '+feature.properties.Address+'</p><p>Source: '+feature.properties.Source+'</p><a href=https://www.loc.gov/item/wa0563/ target=_blank><img src=images/jls.jpg width=125em>');
+        }
+      }).addTo(mymap);
+
+    })
+})

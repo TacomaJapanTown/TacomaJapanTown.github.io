72d97cbbd9eef9ed9a17cdb7f699f75864fb5bb3
TacomaJapanTown <52140649+TacomaJapanTown@users.noreply.github.com>
2019-08-06 08:42:50 -0700

    Add files via upload

--- dailypaths.js
+++ dailypaths.js
@@ -35,16 +35,17 @@ var greenIcon = L.icon({
 //loading the geojson of homes along the paths
 $.getJSON("data/pathstopshomes.geojson",function(data){
   L.geoJson(data, {
-    {icon: greenIcon}
-    onEachFeature: function (feature, layer) {
-      layer.bindPopup('<p>Name: '+feature.properties.Name+'</p><p>Address: '+feature.properties.Address+'</p><p>Notes: '+feature.properties.Notes+'</p><p>Source: '+feature.properties.Source+'</p>');
+  pointToLayer: function(feature,latlng){
+     var marker = L.marker(latlng,{icon: greenIcon});
+     onEachFeature: function (feature, layer) {
+     layer.bindPopup('<p>Name: '+feature.properties.Name+'</p><p>Address: '+feature.properties.Address+'</p><p>Notes: '+feature.properties.Notes+'</p><p>Source: '+feature.properties.Source+'</p>');
+     return marker
     }
   }).addTo(mymap);
 
   //loading the geojson of schools along the paths
   $.getJSON("data/pathstopsschools.geojson",function(data){
     L.geoJson(data, {
-      {icon: greenIcon}
       onEachFeature: function (feature, layer) {
         layer.bindPopup('<p>Name: '+feature.properties.Name+'</p><p>Address: '+feature.properties.Address+'</p><p>Source: '+feature.properties.Source+'</p><p>'+feature.properties.Photo+'</p>');
       }
@@ -53,7 +54,6 @@ $.getJSON("data/pathstopshomes.geojson",function(data){
     //loading the geojson of the JLS
     $.getJSON("data/jls.geojson",function(data){
       L.geoJson(data, {
-        {icon: greenIcon}
         onEachFeature: function (feature, layer) {
           layer.bindPopup('<p>Name: '+feature.properties.Name+'</p><p>Address: '+feature.properties.Address+'</p><p>Source: '+feature.properties.Source+'</p><a href=https://www.loc.gov/item/wa0563/ target=_blank><img src=images/jls.jpg width=125em>');
         }

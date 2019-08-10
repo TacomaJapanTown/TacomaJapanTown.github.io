858c68f21ab1ab7a5b01b8af2150c9b49aedb209
TacomaJapanTown <52140649+TacomaJapanTown@users.noreply.github.com>
2019-08-10 11:40:35 -0700

    Add files via upload

--- japantown.js
+++ japantown.js
@@ -5,7 +5,7 @@
 //https://jsonformatter.curiousconcept.com/
 
 //Making the map
-L.map('mymap').setView([47.256105, -122.443722], 14);
+var mymap = L.map('mapid').setView([47.256105, -122.443722], 14);
 //Basemap
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',

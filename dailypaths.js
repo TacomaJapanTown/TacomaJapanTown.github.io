e28002cb1b51c1e3553eeb3d71483c8b85d9a435
TacomaJapanTown <52140649+TacomaJapanTown@users.noreply.github.com>
2021-05-24 20:38:00 -0700

    Update dailypaths.js

--- dailypaths.js
+++ dailypaths.js
@@ -10,7 +10,7 @@
 //Making the map
 var mymap = L.map('mapid').setView([47.256105, -122.443722], 14);
 //Basemap
-var gray = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+var gray = L.tileLayer('https://api.mapbox.com/styles/v1/sarahp98/cjxo2g3kl2i3x1cmkrhdvjrbw/wmts?access_token=' + L.mapbox.accessToken,', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
   id: 'mapbox.light',
